c67cf8aa7aeed1b6f40019162c4f95a731f16341
Matthew Everett <matthew@mjheverett.com>
2020-10-12 12:49:29 -0400

    added blog pages and routes

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = props => (
+    <>
+        <h1>About Page</h1>
+        <p>Welcome to the About Page.</p>
+        <p>This is all about me.</p>
+    </>
+);
+
+export default About;
